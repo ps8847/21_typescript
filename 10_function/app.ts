024f1a67dc1ffaf360cf274808fdd56d8c30fb41
Prince Sharma <ps789650@gmail.com>
2024-09-09 21:16:17 +0530

    revised ts

--- 10_function/app.ts
+++ 10_function/app.ts
@@ -10,15 +10,15 @@ console.log(Cals());
 //with type
 
 function Cals2():number{
-	// we can't return otehr then the number from here
+	// we can't return other then the number from here
 	return 10+10
 }
 
 console.log(Cals2());
 
-//now the para,eter passed will be accepted only if its number
+//now the parameter passed will be accepted only if its number
 function Cals3(a:number):number{
-	// we can't return otehr then the number from here
+	// we can't return other then the number from here
 	return a;
 }
 
@@ -26,7 +26,7 @@ console.log(Cals3(100));
 
 //now the para,eter passed will be accepted only if its number
 function Cals4(a:number, b?:number):number{
-	// we can't return otehr then the number from here
+	// we can't return other then the number from here
 	return b?a+b:a;
 }
 
